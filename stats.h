6d5fee9fdfdb784ffcdb4aad03e4c67b09e59dfc
VangaSamskruthi1619 <91040719+VangaSamskruthi1619@users.noreply.github.com>
2021-10-21 21:41:45 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,11 @@
 #include <vector>
 
+struct Stats
+{
+        double average;
+        double max;
+        double min;
+};
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<double>& );
 }
