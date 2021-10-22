55be7b66e107810a8294f3061ad0fdecb54afd5d
VangaSamskruthi1619 <91040719+VangaSamskruthi1619@users.noreply.github.com>
2021-10-22 12:36:57 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,10 +2,33 @@
 
 struct Stats
 {
-        double average;
-        double max;
-        double min;
+        double average = 0.0;
+        double max = 0.0;
+        double min = 0.0;
 };
+struct EmailAlert
+{
+        bool emailSent = false;
+}
+struct LEDAlert
+{
+        bool ledGlows = false;
+}
+struct IAlerter
+{
+        bool Alerter = false;
+}
+
+Class StatsAlerter
+{
+        public :
+                StatsAlerter(){}
+                StatsAlerter(const float Maxthreshold,std::vector<IAlerter*> Alert): MaxThreshold(Maxthreshold),alert(Alert){}
+                const float MaxThreshold;
+                std::vector<IAlerter*> alert;
+                void checkAndAlert(const std::vector<double>&);                  
+};           
+
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<double>& );
 }
