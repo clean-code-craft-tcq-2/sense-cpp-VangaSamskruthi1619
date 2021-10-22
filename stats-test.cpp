37f968ffeca771c394260d0bb47ca7457d21e97c
VangaSamskruthi1619 <91040719+VangaSamskruthi1619@users.noreply.github.com>
2021-10-22 17:19:39 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -30,7 +30,12 @@ TEST_CASE("average is NaN for empty array") {
 TEST_CASE("raises alerts when max is greater than threshold") {
     EmailAlert emailAlert;
     LEDAlert ledAlert;
-    std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
+    //std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
+    std::vector<IAlerter*> alerters;
+    IAlerter IA;
+    IA.emailAlert = &emailAlert;
+    IA.ledAlert = &ledAlert;
+    alerters.push_back(IA); 
     
     const float maxThreshold = 10.2;
     StatsAlerter statsAlerter(maxThreshold, alerters);
