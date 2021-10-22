883340ad69fa943e203e202bf3763d26824b7cfa
VangaSamskruthi1619 <91040719+VangaSamskruthi1619@users.noreply.github.com>
2021-10-22 12:36:55 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-Stats Statistics::ComputeStatistics(const std::vector<double>& func ) 
+Stats Statistics::ComputeStatistics(const std::vector<double>& vect ) 
 {
     //Implement statistics here
     Stats sensor1;
@@ -16,10 +16,23 @@ Stats Statistics::ComputeStatistics(const std::vector<double>& func )
     }
     else
     {
-        double avg = std::accumulate(func.begin(), func.end(), 0.0) / func.size();
+        double avg = std::accumulate(vect.begin(), vect.end(), 0.0) / vect.size();
         sensor1.average = round(avg*1000.0)/1000.0;
-        sensor1.max = *std::max_element(func.begin(), func.end()); 
-        sensor1.min = *std::min_element(func.begin(), func.end()); 
+        sensor1.max = *std::max_element(vect.begin(), vect.end()); 
+        sensor1.min = *std::min_element(vect.begin(), vect.end()); 
     }
     return sensor1;  
+}
+void StatsAlerter::checkAndAlert(const std::vector<double>& vect)
+{
+    if(*std::max_element(func.begin(), func.end()) > MaxThreshold)
+    {
+        alert[0].emailSent = true;
+        alert[1].ledGlows = true;
+    }
+    else
+    {
+        alert[0].emailSent = false;
+        alert[1].ledGlows = false;
+    }
 }
